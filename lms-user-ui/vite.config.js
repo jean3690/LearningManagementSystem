9c3e824816b1727c8a56150818b49df8d1fd8811
jean <15259562+jean3690@user.noreply.gitee.com>
2025-10-27 23:00:59 +0800

    项目结构配置中

--- lms-user-ui/vite.config.js
+++ lms-user-ui/vite.config.js
@@ -1,31 +1,59 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Pages from 'vite-plugin-pages'
 import path from 'path'
+import Icon from 'unplugin-icons/vite';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import IconsResolver from 'unplugin-icons/resolver';
+import eslintPlungin from 'vite-plugin-eslint';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueJsx(),
         Pages({
-            passDir: 'src/pages',
-            importPath: '@/pages',
-            extensions: ['vue', 'jsx']
+            dirs: ['src/pages'],
+            extensions: ['vue'],
+            exclude: ['**/components/*.vue'],
         }),
         AutoImport({
-            resolvers: [ElementPlusResolver()],
-            imports: ['vue', 'vue-router']
+            imports: ['vue'],
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    prefix: 'Icon',
+                }),
+            ],
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
-        })
+            resolvers: [
+                ElementPlusResolver(),
+                IconsResolver({
+                    enabledCollections: ['ep']
+                }),
+            ],
+        }),
+        Icon({
+            autoInstall: true,
+        }),
+        eslintPlungin({
+            cache: false,
+            include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
+            exclude: ['node_modules'],
+            fix: true,
+            formatter: 'stylish',
+            failOnError: false,
+            failOnWarning: false,
+        }),
     ],
     alias: {
         '@': path.resolve(__dirname, 'src')
+    },
+    server: {
+        open: true,
+        port: 3000
     }
 })
